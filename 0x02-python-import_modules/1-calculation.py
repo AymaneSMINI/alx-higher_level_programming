1143c19d16e1eae5dbf4576627859493c92a6124
aymanesmini <aymane.smini@gmail.com>
2022-12-01 10:26:46 -0800

    update file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    #print the sum of a and b
+    """ print the sum of a and b"""
     print("{} + {} = {}".format(a, b, add(a, b)))
-    #print the subtraction of a and b
+    """ print the subtraction of a and b"""
     print("{} - {} = {}".format(a, b, sub(a, b)))
-    #print the multiplicationn of a and b
+    """ print the multiplicationn of a and b"""
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    #print the division of a and b
+    """ print the division of a and b """
     print("{} / {} = {}".format(a, b, div(a, b)))
