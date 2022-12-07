ddb4885290d4f84725db6da4beffed945299f33f
aymanesmini <aymane.smini@gmail.com>
2022-12-07 07:53:27 -0800

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,5 @@
 def square_matrix_simple(matrix=[]):
     copy = []
     for row in matrix:
-        copy.append(list(map(lambda x:x*2,row)))
+        copy.append(list(map(lambda x: x*2, row)))
     return copy
-
