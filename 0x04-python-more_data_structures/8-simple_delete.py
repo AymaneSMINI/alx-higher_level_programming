4291cea5615e4de2456eaacb124bce8b55db3efe
aymanesmini <aymane.smini@gmail.com>
2022-12-08 04:41:02 -0800

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    return a_dictionary.pop(key)
+    keys = a_dictionary.keys()
+    if key in keys:
+        a_dictionary.pop(key)
+    return a_dictionary
