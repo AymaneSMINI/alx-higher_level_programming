702e821f25afe9ea44b8428063571d55a5ae4079
aymane <aymane.smini@gmail.com>
2023-01-20 09:01:22 -0800

    accessility of id attribute

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,8 +16,8 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id != None:
-            self.__id = id
+        if id is not None:
+            self.id = id
         else:
             Base.__nb_objects += 1
             Base.id = self.__nb_objects
