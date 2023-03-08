a5f2de157118c1b1d67fe7e1a132d0159305f893
aymane <aymane.smini@gmail.com>
2023-03-08 10:39:32 -0800

    say_my_name function with test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Defines say_my_name  function"""
+
+
+
+def say_my_name(first_name, last_name=""):
+    """ print first name and last name"""
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    say_my_name("Aymane")
+    say_my_name("Aymane", "Smini")
+    say_my_name(12, "White")
