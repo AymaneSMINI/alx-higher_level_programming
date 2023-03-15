2867c1719e39412a168a44a359adadd26d75f930
aymane <aymane.smini@gmail.com>
2023-03-15 05:18:04 -0700

    use process.args to get the erguments from prompt

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (args[2] === undefined) {
+  console.log('No argument');
+} else {
+  let i = 2;
+  while (args[i] !== undefined) {
+    console.log(args[i]);
+    i++; }
+}
