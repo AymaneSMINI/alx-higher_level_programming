607484a4b1bff292c38fb672e28d8c30ceb75646
aymane <aymane.smini@gmail.com>
2023-03-18 07:33:59 -0700

    using array and for loop

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,5 +2,6 @@
 const arg = process.argv;
 if (isNaN(arg[2]) === true || arg[2] === undefined) {
   console.log('Not a number');
-} else 
-  console.log('My number: '+ parseInt(arg[2]));
+} else {
+  console.log('My number: ' + parseInt(arg[2]));
+}
