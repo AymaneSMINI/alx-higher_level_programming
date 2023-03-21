f0b51f3c34b4e8e161e73ab765140a90763e74b2
aymane <aymane.smini@gmail.com>
2023-03-21 11:37:42 -0700

    update square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const size = process.argv[2];
-if (size === undefined && isNaN(size) === true) {
+if (size === undefined || isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
