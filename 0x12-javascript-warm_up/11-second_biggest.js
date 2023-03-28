f41ed10011296c7dd316316815843a7cea4c6223
aymane <aymane.smini@gmail.com>
2023-03-28 11:24:15 -0700

    add reverse function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ const numbers = process.argv.slice(2);
 if (numbers.length <= 1) {
   console.log(0);
 } else {
-  console.log(numbers.map(str => parseInt(str)).sort()[1]);
+  console.log(numbers.map(str => parseInt(str)).sort().reverse()[1]);
 }
