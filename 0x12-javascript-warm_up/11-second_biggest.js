4bd659cc12b4c410346e31b7ed7cd85d085ec13d
AYMANE SMINI <91330646+AymaneSMINI@users.noreply.github.com>
2023-05-05 10:10:33 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const numbers = process.argv.slice(2);
-if (numbers.length <= 1) {
-  console.log(0);
-} else {
-  console.log(numbers.map(str => parseInt(str)).sort().reverse()[1]);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv;
-if (isNaN(arg[2]) === true || arg[2] === undefined) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(arg[2]));
-}
