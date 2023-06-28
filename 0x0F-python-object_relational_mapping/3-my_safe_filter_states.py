3c0d03a78940f147dde1a5da2dfa8b0d4f4ed1b4
aymanesmini <aymane.smini@gmail.com>
2023-06-28 05:07:00 -0700

    fixing some ugs

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,11 +5,11 @@ import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], \
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE 
+    cur.execute("SELECT * FROM states WHERE \
                 states.name= %s ORDER BY  states.id", (match,))
     query_rows = cur.fetchall()
     for row in query_rows:
