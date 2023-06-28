30a7fb7478eac87bcb4d5c799d774df6652f7b7e
aymanesmini <aymane.smini@gmail.com>
2023-06-28 03:48:10 -0700

    sql command using MySQLdb library

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  lists all states starting with upper N from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+cur = conn.cursor()
+match = sys.argv[4]
+cur.execute("SELECT * FROM states WHERE states.name = %s ORDER BY states.id",(match,))
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
