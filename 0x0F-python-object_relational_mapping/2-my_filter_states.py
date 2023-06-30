24973b71a3fd6f5a9f7879fcddb2131633aba10d
aymanesmini <aymane.smini@gmail.com>
2023-06-30 01:52:44 -0700

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states starting with the name inserted as an argument from the database hbtn_0e_0_usa """
+"""  lists all states starting with the name inserted
+as an argument from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2],
-                           db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name='{}' \
-                ORDER BY states.id".format(sys.argv[4]))
+    cur.execute("""
+        SELECT * FROM states
+        WHERE states.name='{}'
+        ORDER BY states.id""".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states starting with safe insarted name from the database hbtn_0e_0_usa """
+"""  lists all states starting with safe inserted name 
+from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], \
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8")
     cur = conn.cursor()
     match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE \
-                states.name= %s ORDER BY  states.id", (match,))
+    cur.execute("""
+        SELECT * FROM states WHERE
+        states.name= %s ORDER BY  states.id""", (match,))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
