bd3656b096d37b158430314522b314ec1de5a836
aymane <aymane.smini@gmail.com>
2024-02-10 04:52:33 -0800

    add starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const id =  process.argv[2];
-request.get(`https://swapi-api.alx-tools.com/api/films/${id}`).on('response', function (response, body) {
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, function (error, response, body) {
   const responseJSON = JSON.parse(body);
   console.log(responseJSON.title);
 });
